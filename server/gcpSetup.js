9ad9388d69edb29ea028f47de97db55eb7b84058
Nidhin Dev Vrindavan <nidhindev@gmail.com>
2020-04-04 00:16:40 +0200

    tweek

---
+++ server/gcpSetup.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+const properties = {
+    gcpData: process.env.GCP_DATA || JSON.stringify({
+       
+    }),
+    gcpFile: process.env.GCP_KEY_FILE || './src/google-sheet/gcpconfig.json'
+};
+fs.writeFile(properties.gcpFile, properties.gcpData, (err) => { console.log(err)});
