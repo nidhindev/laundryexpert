f47b051b118e5ef095da5cec39e375d4836d6763
Nidhin Dev Vrindavan <nidhindev@gmail.com>
2020-04-06 22:40:53 +0200

    adding cache

--- server/src/google-sheet/googleSheetService.js
+++ server/src/google-sheet/googleSheetService.js
@@ -1,7 +1,10 @@
 var { JWT } = require('google-auth-library');
 const keys = require('./gcpconfig.json');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
+const NodeCache = require("node-cache");
+
 const sheets = google.sheets('v4');
+const gcpChache = new NodeCache({ stdTTL: 3500, checkperiod: 3600, });
 
 const spreadsheetId = '17cYZqSLhHOpvP5T27dsv8A3Rk9E6-iHCH7q8uaTs5C8'
 async function getSheet() {
@@ -16,29 +19,32 @@ const keys = JSON.parse(keysEnvVar);
   const client = auth.fromJSON(keys);
 
      */
-    const client = new JWT(
-        keys.client_email,
-        null,
-        keys.private_key,
-        ['https://www.googleapis.com/auth/spreadsheets'],
-      );
-      
-      const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}`;
-      const res = await client.request({url});
-      console.log(client);
-
-      sheets.spreadsheets.values.get({
+    var client = null;
+    if (gcpChache.has("gcpClient")) {
+        console.log('chache')
+        client = gcpChache.get('gcpClient')
+    } else {
+        console.log('new');
+        client = new JWT(
+            keys.client_email,
+            null,
+            keys.private_key,
+            ['https://www.googleapis.com/auth/spreadsheets'],
+        );
+        gcpChache.set("gcpClient", client);
+    }
+    sheets.spreadsheets.values.get({
         auth: client,
         spreadsheetId: spreadsheetId,
         range: 'Sheet1'
-     }, function (err, response) {
+    }, function (err, response) {
         if (err) {
             console.log('The API returned an error: ' + err);
         } else {
             console.log('Movie list from Google Sheets:', JSON.stringify(response));
-           
+
         }
-     });
+    });
 }
 
 
