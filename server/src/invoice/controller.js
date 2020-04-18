d22e81d18e44b603c08fab618f5b40ac2c031b30
Devayan Sarkar <devayansarkar@gmail.com>
2020-04-18 08:28:50 +0200

    Adding template generation

---
+++ server/src/invoice/controller.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {generatePdf} = require('./invoiceGenerator')
+const path = require('path')
+router.get('/invoice/generate', (req, res) => {
+    generatePdf()
+    res.send({
+        status: 'ok'
+    }).status(200);
+});
+router.get('/invoice/', (req, res) => {
+    generatePdf()
+    const invoicePath = path.resolve("dist/invoice.pdf");
+    res.sendFile(invoicePath)
+});
+module.exports = router;
