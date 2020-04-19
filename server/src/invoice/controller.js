49e23df49d49a344ccc3e7adc2f9ac188fb9f923
abbinv@gmail.com <abbinv@gmail.com>
2020-04-19 12:15:59 +0200

    Changes for uploading to google drive (Credentials has to be entered seperately)

--- server/src/invoice/controller.js
+++ server/src/invoice/controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {generatePdf} = require('./invoiceGenerator')
+const {upload} = require('./googledriveupload')
 const path = require('path')
 router.get('/invoice/generate', (req, res) => {
     generatePdf()
@@ -8,6 +9,14 @@ router.get('/invoice/generate', (req, res) => {
         status: 'ok'
     }).status(200);
 });
+
+router.get('/invoice/upload', (req, res) => {
+    upload()
+    res.send({
+        status: 'ok'
+    }).status(200);
+});
+
 router.get('/invoice/', (req, res) => {
     const invoicePath = path.resolve("dist/invoice.pdf");
     res.sendFile(invoicePath)
