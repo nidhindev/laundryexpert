a66224c735ec20c8f0251115c6038d1f3b88cf98
Nidhin Dev Vrindavan <nidhindev@gmail.com>
2020-04-25 14:12:42 +0200

    centralized caching in all services

--- server/src/google-sheet/googleSheetUpdateService.js
+++ server/src/google-sheet/googleSheetUpdateService.js
@@ -1,34 +1,16 @@
-var { JWT } = require('google-auth-library');
 const { google } = require('googleapis');
-const NodeCache = require("node-cache");
-const { spreadsheetId, creds } = require('../../config');
-const sheets = google.sheets('v4');
-const gcpChache = new NodeCache({ stdTTL: 3500, checkperiod: 3600, });
+const { spreadsheetId } = require('../../config');
+const { gcpChache } = require("../cache/appCache");
 
 async function updateSheet(body) {
-    const selectedStore = body.store
-    var keys = JSON.parse(creds);
-    if (!keys) {
-        throw new Error('The $CREDS environment variable was not found!');
-    }
-    var client = null;
-    if (gcpChache.has(`gcpClient-${selectedStore}`)) {
-        client = gcpChache.get(`gcpClient-${selectedStore}`)
-    } else {
-        console.log('calling google auth')
-        client = new JWT(
-            keys.client_email,
-            null,
-            keys.private_key,
-            ['https://www.googleapis.com/auth/spreadsheets'],
-        );
-        gcpChache.set(`gcpClient-${selectedStore}`, client);
-    }
+    const selectedStore = body.store;
+    const sheets = google.sheets('v4');
+    const client = gcpChache.get('gcpClient');
     const sheetResponse = await sheets.spreadsheets.values.get({
         auth: client,
         spreadsheetId: spreadsheetId,
         range: selectedStore
-    })
+    });
     const request = {
         spreadsheetId: spreadsheetId,
         range: `${selectedStore}!A${sheetResponse.data.values.length + 1}:L${sheetResponse.data.values.length + 1 + body.values.length}`,  // TODO: Update placeholder value.
