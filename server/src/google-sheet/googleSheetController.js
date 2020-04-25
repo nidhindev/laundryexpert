7206ab6ed7794137d0ab20609a930fc8b2702d87
Nidhin Dev Vrindavan <nidhindev@gmail.com>
2020-04-25 14:15:47 +0200

    changing file name

--- server/src/google-sheet/googleSheetController.js
+++ server/src/google-sheet/googleSheetController.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const googleSheetService = require('./googleSheetService');
+const googleSheetFindService = require('./googleSheetFindService');
 const googleSheetUpdateService = require('./googleSheetUpdateService')
 const googleSheetPricingService = require('./googleSheetPricingService')
 router.get('/customer', (req, res) => {
-  googleSheetService.getSheet(req.query.id, req.query.selectedStore, req.query.searchBy).then(result => {
+  googleSheetFindService.getSheet(req.query.id, req.query.selectedStore, req.query.searchBy).then(result => {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(200);
