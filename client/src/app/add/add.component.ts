4e9ee242f105e76b187a1de86bba01fdbe5166a0
Nidhin Dev Vrindavan <nidhindev@gmail.com>
2020-04-26 10:35:28 +0200

    remark filtering

--- client/src/app/add/add.component.ts
+++ client/src/app/add/add.component.ts
@@ -60,7 +60,7 @@ export class AddComponent implements OnInit {
       shopName: [],
       date: [Date.now()]
     });
-    this.googleSheetService.getPricing().subscribe(result => {
+    this.googleSheetService.getBusinessConfig().subscribe(result => {
       for (let entry of JSON.parse(JSON.stringify(result)).pricing) {
         this.itemOptions.push(entry.key)
       }
@@ -80,7 +80,7 @@ export class AddComponent implements OnInit {
       this.notReady = false
     });
     this.itemNameSubscription = this.item.get("item").valueChanges.subscribe(val => {
-      this.googleSheetService.getPricing().subscribe(result => {
+      this.googleSheetService.getBusinessConfig().subscribe(result => {
         this.priceList = JSON.parse(JSON.stringify(result)).pricing
         let priceObject = JSON.parse(JSON.stringify(result)).pricing.find(config => config.key == val)
         if (priceObject) {
