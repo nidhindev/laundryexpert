96ac3c5145e7e3c0df9f1448d2e11ebde6a2166a
Devayan Sarkar <devayansarkar@gmail.com>
2020-05-03 17:41:03 +0200

    removed cross origin scripts

--- server/src/order/orderController.js
+++ server/src/order/orderController.js
@@ -7,39 +7,29 @@ const orderUpdateServiceV2 = require('./orderUpdateServiceV2');
 
 router.get('/customer', (req, res) => {
   orderRetrivalService.getSheet(req.query.id, req.query.selectedStore, req.query.searchBy).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(200);
   });
 });
 router.put('/update', (req, res) => {
   orderUpdateService.updateSheet(req.body).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(200);
   });
 });
 
 router.get('/archive', (req, res) => {
   orderArchiveService.archiveSheet('testshop').then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(200);
   });
 });
 
 router.get('/order/v2', (req, res) => {
   orderRetrivalService.getOrders(req.query.storeName, req.query.orderNumber, req.query.customerPhone).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(200);
   });
 });
 
 router.post('/order/v2', (req, res) => {
   orderUpdateServiceV2.createOrder(req.body).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(result.status);
   });
 });
@@ -47,8 +37,6 @@ router.post('/order/v2', (req, res) => {
 
 router.put('/order/v2', (req, res) => {
   orderUpdateServiceV2.updateOrder(req.body).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(result.status);
   });
 });
@@ -57,8 +45,6 @@ router.put('/order/v2', (req, res) => {
 // eg: /order/v2/status?storeName=testsheet&status=DELIVERED&orderNumber=1002
 router.put('/order/v2/status', (req, res) => {
   orderUpdateServiceV2.updateOrderAsDelivered(req.query.storeName, req.query.orderNumber, req.query.status).then(result => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.json(result).status(result.status);
   });
 });
